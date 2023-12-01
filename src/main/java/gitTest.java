9c324c8996c22f14e4317b02918fad6283eac4c1
阿松 <1254084288@qq.com>
2023-12-01 20:42:33 +0800

    123123

---
+++ src/main/java/gitTest.java
@@ -0,0 +1,10 @@
+import com.sun.xml.internal.ws.api.model.wsdl.WSDLOutput;
+
+import java.sql.SQLOutput;
+
+public class gitTest {
+
+    public void Test(){
+        System.out.println("成了");
+    }
+}
